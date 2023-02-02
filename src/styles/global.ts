666d50dd7fbc4ad1b0276db144df0cf57837c89e
Miqueias Sousa <miqueiassousa80@gmail.com>
2023-02-02 16:52:27 -0300

    fix: change focus border color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${props => props.theme.colors.yellow[900]};
+    box-shadow: 0 0 0 2px ${props => props.theme.colors.purple[500]};
   }
 
   body {
