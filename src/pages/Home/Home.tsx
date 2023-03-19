82b9611bc62a5b7201b6899fe7ca95a537a465fd
Miqueias Sousa <miqueiassousa80@gmail.com>
2023-03-19 10:51:47 -0300

    refactor: delete Banner component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,21 @@
 import { useTheme } from 'styled-components'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 import { CoffeeCard } from '../../components/CoffeeCard/CoffeeCard'
+import { Stack } from '../../components/Stack'
 import { Title } from '../../components/Title'
+import bannerImage from '../../assets/banner.png'
 
-import { Banner } from './components/Banner/Banner'
-import { Feed, FeedContainer } from './Home.styles'
+import {
+  BannerContainer,
+  IconContainer,
+  Item,
+  ItemContainer,
+  Subtitle,
+  Wrapper,
+  Feed,
+  FeedContainer
+} from './Home.styles'
 
 const coffeeList = [
   {
@@ -160,7 +171,46 @@ export function Home() {
 
   return (
     <main>
-      <Banner />
+      <BannerContainer>
+        <Wrapper>
+          <Stack spacing={2}>
+            <Title variant="xl">
+              Encontre o café perfeito para qualquer hora do dia
+            </Title>
+            <Subtitle>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </Subtitle>
+          </Stack>
+          <ItemContainer>
+            <Item>
+              <IconContainer className="cart">
+                <ShoppingCart size={16} weight="fill" />
+              </IconContainer>
+              <span>Compra simples e segura</span>
+            </Item>
+            <Item>
+              <IconContainer className="package">
+                <Package size={16} weight="fill" />
+              </IconContainer>
+              <span>Embalagem mantém o café intacto</span>
+            </Item>
+            <Item>
+              <IconContainer className="timer">
+                <Timer size={16} weight="fill" />
+              </IconContainer>
+              <span>Entrega rápida e rastreada</span>
+            </Item>
+            <Item>
+              <IconContainer className="coffee">
+                <Coffee size={16} weight="fill" />
+              </IconContainer>
+              <span>O café chega fresquinho até você</span>
+            </Item>
+          </ItemContainer>
+        </Wrapper>
+        <img src={bannerImage} />
+      </BannerContainer>
       <FeedContainer spacing={18}>
         <Title as="h2" variant="lg" color={theme.colors.base.subtitle}>
           Nossos cafés
